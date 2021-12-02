22fc8104360bf8ed73d8f820a4fd4a493798f744
smhrd <smhrd@DESKTOP-SORR6DC>
2021-12-02 16:38:42 +0900

    ㅇㅇㄹ

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
-<<<<<<< HEAD
+
    public static void main(String[] args) {
       // Github 환경설정
       //1. JDK 버전
@@ -13,19 +13,3 @@ public class Test {
       //1111111111111111
       //222222222222222
    }
-
-=======
-
-   public static void main(String[] args) {
-	   
-      // Github 환경설정
-      //1. JDK 버전
-      //2. tomcat 버전
-      //3. encodig  방식 (UTF-8)
-      // 테스트
-	   
-		System.out.println("First Code");
-		// 계속 확인
-	}
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-5/jalhanda.git
-}
