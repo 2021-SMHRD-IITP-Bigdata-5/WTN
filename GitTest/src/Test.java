59572621b0541a8dc6da54cd88a49c912487743e
smhrd <smhrd@DESKTOP-SORR6DC>
2021-12-02 16:37:22 +0900

    안녕

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -1,10 +1,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// Github 환경설정
-		//1. JDK 버전
-		//2. tomcat 버전
-		//3. encodig 방식 (UTF-8)
+		//異⑸룎
 		
 
 		System.out.println("First Code");
