b3fad54e29b165e4b4fc98bfe477f55c3913feb8
smhrd <smhrd@DESKTOP-FV8SRRK>
2021-12-02 15:50:55 +0900

    충돌1

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -1,7 +1,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-
+		// Github 환경설정
+		//1. JDK 버전
+		//2. tomcat 버전
+		//3. encodig 방식 (UTF-8)
 		System.out.println("First Code");
 		
 	}
