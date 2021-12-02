0d447fe38a28433c8df7376466a759a912ebe27d
smhrd <smhrd@DESKTOP-JO07ENF>
2021-12-02 16:42:41 +0900

    테스트

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -1,15 +1,19 @@
 public class Test {
 
-	public static void main(String[] args) {
-		// Github 환경설정
-		//1. JDK 버전
-		//2. tomcat 버전
-		//3. encodig  방식 (UTF-8)
-		
+   public static void main(String[] args) {
+      // Github 환경설정
+      //1. JDK 버전
+      //2. tomcat 버전
+      //3. encodig  방식 (UTF-8)
+      
 
-		System.out.println("First Code");
-		// 계속 확인
-	
-	}
+      System.out.println("First Code");
+      // 계속 확인
+      //1111111111111111
+      //222222222222222
+      //333333333333
+      //44444444444444
+      //5555555555555
+   }
 
 }
